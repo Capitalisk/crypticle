ff115ed4e43d5f217578b1498c7190063482764d
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-06-02 00:10:36 +0200

    Improve documentation and add documentation to home screen

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Crypticle
 
-A microservice which lets users convert value between blockchain tokens and pegged credit which can be spent on a centralized service/platform.
-The credit is entrusted to the service provider and is pegged to the value of the underlying blockchain tokens - This credit can be used to support high-thoughput off-chain transactions (related to a specific service). Any unused credit can be converted back into trustless blockchain tokens at any time.
+A multi-tenant hot wallet manager and off-chain payment microservice. Crypticle lets users convert value between blockchain tokens and pegged credit which can be efficiently spent and/or transferred between accounts within the context of a centralized service/platform. The credit is entrusted to the service provider and is pegged to the value of the underlying blockchain tokens - This credit can be used to support high-thoughput off-chain transactions (related to a specific service). Any unused credit can be converted back into trustless blockchain tokens at any time.
 
 The goal of this project is to automate the payment and accounting layer for SaaS services and platforms using blockchain technology.
 By reducing the friction involved in converting back and forth between trustless blockchain tokens and entrusted credit, users can choose their level of risk exposure when using a paid service from an unfamiliar third-party.
@@ -16,3 +15,8 @@ Some potential use cases:
 - Games
 
 After a Crypticle node has been attached to a specific Blockchain and has started accepting deposits from users, it becomes difficult for a service provider to move away from that blockchain without violating the implicit agreement that they have with their users. Attaching services to a specific blockchain using Crypticle should therefore help to create sustainable demand for the underlying blockchain token.
+
+## FAQs
+
+Q: Doesn't this go against the principles behind blockchain and decentralization?
+A: Crypticle aims to be decentralized but not trustless. The core philosophy behind Crypticle is that money should be stored safely (trustlessly) but spent efficiently (trustfully). Every time an entity sends money to another entity, the payer must implicitly trust that the payee will deliver a certain amount of value (e.g. good or service) in return; it's possible that the payee could receive the money but not deliver the good or service. In the context of a marketplace, the risk of a bad actor can never be fully mitigated. Crypticle is a pragmatic solution which allow cryptocurrency users to make unlimited free off-chain transactions with a service provider that they trust. As a user, you can deposit a limited amount of funds directly on the provider's Crypticle service wallet but the actual payments will happen off-chain directly with the provider. Unused funds can be withdrawn back to the safety of your blockchain wallet at any time (assuming that the provider is operating a legitimate service).
--- public/pages/page-home.js
+++ public/pages/page-home.js
@@ -10,7 +10,87 @@ function getPageComponent(pageOptions) {
     template: `
       <div class="page-container container is-fullhd">
         <div class="container is-fullhd">
-          Coming soon...
+          <h4 class="title is-4">API overview</h4>
+
+          <p>
+            Crypticle exposes a realtime WebSocket API for performing transactions and listening for realtime changes.
+            The API adheres the <a href="https://github.com/SocketCluster/socketcluster/blob/master/socketcluster-protocol.md#socketcluster-protocol-v1">SocketCluster protocol</a>.
+            The following examples make use of the <a href="https://github.com/SocketCluster/asyngular-client">Asyngular JavaScript client</a>.
+          </p>
+
+          <hr class="hr hr-medium-spacing" />
+
+          <h4 class="title is-4">Account RPCs</h4>
+          <h5 class="title is-5">Transfer</h5>
+          <pre class="code-snippet"><code>
+    socket.invoke('transfer', {
+      amount: '1000000000',
+      toAccountId: '18b50e59-f3a1-4b57-8f0b-7daeba7259ad',
+      data: 'Notes...'
+    })
+          </code></pre>
+          <ul class="list">
+            <li class="list-item"><code>amount</code> is the amount of funds to send to the specified Crypticle account - It is expressed in the smallest possible cryptocurrency unit.</li>
+            <li class="list-item"><code>toAccountId</code> is the ID of the Crypticle account to send the funds to.</li>
+            <li class="list-item"><code>data</code> is a custom string to add to both the debit and credit transactions which will be created as a result of the transfer.</li>
+          </ul>
+          <div class="spacer"></div>
+          <h5 class="title is-5">Withdrawal</h5>
+          <pre class="code-snippet"><code>
+    socket.invoke('withdraw', {
+      amount: '1100000000',
+      toWalletAddress: '6942317426094516776R'
+    })
+          </code></pre>
+          <ul class="list">
+            <li class="list-item"><code>amount</code> is the amount of funds to withdraw from your Crypticle account - It is expressed in the smallest possible cryptocurrency unit.</li>
+            <li class="list-item"><code>toWalletAddress</code> is the blockchain wallet address to send the funds to.</li>
+          </ul>
+          <div class="spacer"></div>
+          <h5 class="title is-5">Deposit</h5>
+          <p>
+            To make a deposit, send a blockchain transaction to the deposit address of your Crypticle account (as shown on your console dashboard). The deposit wallet address for your account is shown on your console dashboard.
+          </p>
+        </div>
+
+        <hr class="hr hr-medium-spacing" />
+
+        <div class="container is-fullhd">
+          <h4 class="title is-4">Admin RPCs</h4>
+          <h5 class="title is-5">Transfer</h5>
+          <pre class="code-snippet"><code>
+    socket.invoke('adminTransfer', {
+      amount: '20000000',
+      fromAccountId: '213288af-9239-494d-844d-d064ced6f9ea',
+      toAccountId: '18b50e59-f3a1-4b57-8f0b-7daeba7259ad',
+      data: 'Notes...'
+    })
+          </code></pre>
+          <ul class="list">
+            <li class="list-item"><code>amount</code> is the amount of funds to send to the specified Crypticle account - It is expressed in the smallest possible cryptocurrency unit.</li>
+            <li class="list-item"><code>fromAccountId</code> is the ID of the Crypticle account from which to take the funds from.</li>
+            <li class="list-item"><code>toAccountId</code> is the ID of the Crypticle account to send the funds to.</li>
+            <li class="list-item"><code>data</code> is a custom string to add to both the debit and credit transactions which will be created as a result of the transfer.</li>
+          </ul>
+          <div class="spacer"></div>
+          <h5 class="title is-5">Withdrawal</h5>
+          <pre class="code-snippet"><code>
+    socket.invoke('adminWithdraw', {
+      amount: '234000000',
+      fromAccountId: '7667174938767705051R',
+      toWalletAddress: '6942317426094516776R'
+    })
+          </code></pre>
+          <ul class="list">
+            <li class="list-item"><code>amount</code> is the amount of funds to withdraw from the specified Crypticle account - It is expressed in the smallest possible cryptocurrency unit.</li>
+            <li class="list-item"><code>fromAccountId</code> is the ID of the Crypticle account from which to withdraw the funds.</li>
+            <li class="list-item"><code>toWalletAddress</code> is the blockchain wallet address to send the funds to.</li>
+          </ul>
+          <div class="spacer"></div>
+          <h5 class="title is-5">Deposit</h5>
+          <p>
+            To make a deposit, send a blockchain transaction to the deposit address of the relevant Crypticle account.
+          </p>
         </div>
       </div>
     `
