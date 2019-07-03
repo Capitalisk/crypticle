ccd2f16422c672e6575fb8d130119ce468a5122d
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-07-04 01:02:41 +0200

    Working Lisk adapter

--- blockchains/lisk/adapter.js
+++ blockchains/lisk/adapter.js
@@ -4,9 +4,8 @@ const { APIClient: LiskApiClient } = require('@liskhq/lisk-api-client');
 const LiskTransactions = require('@liskhq/lisk-transactions');
 
 class LiskAdapter {
-
   constructor(options) {
-    this.client = new LiskApiClient([options.nodeAddress])
+    this.client = new LiskApiClient([options.nodeAddress]);
   }
 
   generateWallet() {
@@ -19,7 +18,7 @@ class LiskAdapter {
       passphrase,
       privateKey: Buffer.from(privateKey).toString('hex'),
       publicKey: Buffer.from(publicKey).toString('hex'),
-      address,
+      address
     };
   }
 
@@ -52,8 +51,16 @@ class LiskAdapter {
     return (await this.client.accounts.get({address: walletAddress})).data[0].balance;
   }
 
-  signTransaction(transaction, passphrase) {
-    return LiskTransactions.signTransaction(transaction, passphrase);
+  async signTransaction(transaction, passphrase) {
+    let liskTransaction = {
+      type: 0,
+      amount: transaction.amount,
+      recipientId: transaction.recipient,
+      fee: await this.fetchFees(),
+      asset: {},
+      senderPublicKey: LiskCryptography.getAddressAndPublicKeyFromPassphrase(passphrase).publicKey
+    };
+    return LiskTransactions.utils.prepareTransaction(liskTransaction, passphrase);
   }
 
   async sendTransaction(signedTransaction) {
